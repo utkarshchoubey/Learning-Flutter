457bc58bf470b30871abbe63a7bb0fce51bf0eaf
utkarshchoubey <utkarshchoubey629@gmail.com>
2019-05-29 16:32:38 +0530

    Add login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,110 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
+void main()
+{
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: "My App",
-      home: new HomePage(),
+      home: new LoginPage(),
       theme: new ThemeData(
-        primarySwatch: Colors.green,
-        brightness:Brightness.light,
-        accentColor: Colors.red,
+        primarySwatch: Colors.blue,
+        
       ),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
+class LoginPage extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  _LoginPageState createState() => _LoginPageState();
 }
-class _HomePageState extends State<HomePage> {
-  String myText="Hello World";
-  void _changeText(){
-    setState(() {
-      if(myText.startsWith("H")){
-        myText="Welcome to my page";
-      }
-      else{
-        myText="Hello World";
-      }
 
-    });
-  }
+class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin{
 
-  Widget _bodyWidget(){
-    return new Container(
-      padding: const EdgeInsets.all(8.00),
-      child: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(myText,
-            style: new TextStyle(
-              color: Colors.black,
-              fontSize: 20.0,
-              fontStyle: FontStyle.italic
-            ),),
-          ],
-        ),
-      ),
-    );
+  AnimationController _iconAnimationController;
+  Animation<double> _iconAnimation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _iconAnimationController = new AnimationController(vsync:this,duration: new Duration(milliseconds: 500));
+    _iconAnimation = new CurvedAnimation(parent: _iconAnimationController, curve: Curves.bounceOut);
+    _iconAnimation.addListener(()=>this.setState((){}));
+    _iconAnimationController.forward();
+
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: AppBar(
-        title: new Text("HomePage"),
+      appBar: AppBar(title: new Text("Login Page"),),
+      backgroundColor: Colors.black,
+      body: new Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          new Image(image: new AssetImage("assets/backg.jpg"),
+          fit: BoxFit.cover,
+            color: Colors.black87,
+            colorBlendMode: BlendMode.darken,
+          ),
+          new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new FlutterLogo(size: _iconAnimation.value * 100),
+              new Form(
+                  child: Theme(
+                    data: ThemeData(
+                      primarySwatch: Colors.teal,
+                      brightness: Brightness.dark,
+                      inputDecorationTheme: new InputDecorationTheme(
+                        labelStyle: new TextStyle(color: Colors.teal,fontSize: 20.0)
+                      )
+                    ),
+                    child: Container(
+                      padding: EdgeInsets.all(35.0),
+                      child: new Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          TextFormField(
+                            decoration: new InputDecoration(
+                                labelText: "Enter Email"
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+
+
+                          ),
+                          TextFormField(
+                            decoration: new InputDecoration(
+                                labelText: "Enter Password"
+                            ),
+                            keyboardType: TextInputType.text,
+                            obscureText: true,
+                          ),
+                          new Padding(padding: EdgeInsets.only(top: 40.0)),
+                          new MaterialButton(
+                            height: 40.0,
+                            minWidth: 100.0,
+                            color: Colors.teal,
+                            textColor: Colors.white,
+                            child: new Icon(Icons.arrow_right),
+                            onPressed: () => {},
+                            splashColor: Colors.red,
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+              )
+
+            ],
+          )
+        ],
       ),
-      body: _bodyWidget(),
-        floatingActionButton: new FloatingActionButton(
-          child: new Icon(Icons.add),
-          onPressed: _changeText,
-        ),
     );
   }
 }
-
